2275f45e7249d761dfd0f1cde4a79a62ca716952
Valentina Colmenares <vcolmenares@unal.edu.co>
2024-07-23 22:21:09 -0500

    Enteros y flotantes

--- src/Paquete/HolaMundo.java
+++ src/Paquete/HolaMundo.java
@@ -1,10 +1,14 @@
 package Paquete;
 
+// Tema 1: Conceptos básicos
+
 public class HolaMundo {
 
     public static void main(String[] args) {
 
         System.out.println("¡Hola Mundo!");
+        String mensaje = "Hola Mundo";
+        System.out.println(mensaje);
 
     }
 
---
+++ src/Paquete/Variables.java
@@ -0,0 +1,40 @@
+package Paquete;
+
+// Tema 2: Variables y tipos de datos
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        // Números enteros
+
+        // byte: Emplea un sólo byte (8 bits) de almacenamiento.
+        // Esto permite almacenar valores entre [-128, 127].
+        byte numeroByte = 9;
+
+        // short: Emplea el doble almacenamiento de (byte).
+        // Esto permite almacenar valores entre [-32.768, 32.767].
+        short numeroShort = 32767;
+
+        // int: Emplea un tamaño mayor, 4 bytes (32 bits).
+        // Esto permite almacenar valores entre [-2147483648, 2147483647].
+        int numeroInt = 41825;
+
+        // long: Emplea el tamaño mayor de todos los enteros, 8 bytes (64 bits).
+        // Esto permite almacenar valroes entre [-9223372036854775808, 9223372036854775807].
+        long numeroLong = 926465464697266565L;
+
+        // Números flotantes
+
+        //float: Emplea un tamaño de 32 bits (4 bytes).
+        //Esto permite almacenar valores entre [-3.4028234+8, 1.40239846E-45].
+        float numeroFloat = 5976464F;
+
+        //double: Emplea un tamaño de 64 bits (8 bytes).
+        //Esto permite almacenar valores entre [-1.7976931348623157E+309, 4.94065645841246544E-324]
+        double numerDouble =2654792142478F;
+
+        // Booleanos y caracteres
+    }
+
+}
